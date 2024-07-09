293ca1e4878ccd42f303f6daf5a79ba2c14273ec
André Rodrigues <35631672+andrerodrigues0018@users.noreply.github.com>
2024-07-09 22:54:39 +0000

    fix: remove layout file from project

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,8 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Menu />
-      {children}</body>
+        <Menu/>
+        {children}
+      </body>
     </html>
   );
 }
